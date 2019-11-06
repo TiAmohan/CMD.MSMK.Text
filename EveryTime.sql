7334d3aa83538480d5a089acbf9fe746c3506245
wangbenchi66 <69945864@qq.com>
2019-11-06 15:54:03 +0800

    添加每张表的存储过程(个别表除外)

--- EveryTime.sql
+++ EveryTime.sql
@@ -1,149 +1,188 @@
 use master
 go
-if exists(select * from sys.databases where name='EveryTime')--每时每刻数据库
+
+
+--每时每刻数据库
+if exists(select * from sys.databases where name='EveryTime')
 drop database EveryTime
 go
-create database EveryTime
+	create database EveryTime
 go
 use EveryTime
 go
-if exists(select * from sys.tables where name='Users')--用户表
-drop table Users
+
+--用户表
+if exists(select * from sys.tables where name='Users')
+	drop table Users
 go
 create table Users
 (
-Userid int primary key identity(1,1),--用户id
-Username varchar(50) not null,       --用户名
-Userpassword varchar(50) not null,   --用户密码
-Usergrade int check(Usergrade=1
- or Usergrade=2 or Usergrade=3 
- or Usergrade=4) not null,           --用户等级(1为买家，2为卖家，3为供应商，4为平台管理)
-Userbalance money not null           --用户余额
+	Userid int primary key identity(1,1),--用户id
+	Username varchar(50) not null,       --用户名
+	Userpassword varchar(50) not null,   --用户密码
+	Usergrade int check(Usergrade=1
+	 or Usergrade=2 or Usergrade=3 
+	 or Usergrade=4) not null,           --用户等级(1为买家，2为卖家，3为供应商，4为平台管理)
+	Userbalance money not null           --用户余额
 )
-if exists(select * from sys.tables where name='Provider')--供应商表
-drop table Provider
+go
+
+--供应商表
+if exists(select * from sys.tables where name='Provider')
+	drop table Provider
 go
 create table Provider
 (
-Providerid int primary key identity(1,1),--供应商id
-Providername varchar(50),                --供应商名称
-Providerstate int default(0) check(Providerstate=0 or Providerstate=1)not null,    --(供应商状态默认是0，0是正常，1是冻结)
+	Providerid int primary key identity(1,1),--供应商id
+	Providername varchar(50),                --供应商名称
+	Providerstate int default(0) check(Providerstate=0 or Providerstate=1)not null,    --(供应商状态默认是0，0是正常，1是冻结)
 )
-if exists(select * from sys.tables where name='ProviderProduct')--供应商商品表
-drop table ProviderProduct
+go
+
+--供应商商品表
+if exists(select * from sys.tables where name='ProviderProduct')
+	drop table ProviderProduct
 go
 create table ProviderProduct
 (
-Productid int primary key identity(1,1),--供应商商品id
-Productname varchar(50)not null,        --供应商商品名称
-Productcontent varchar(100)not null,    --供应商商品简介
-Productprice varchar(100)not null,      --供应商商品价格
-Productimg varchar(50) not null,        --供应商商品图片
-Providerid int references Provider(Providerid)--对应供应商表的供应商id（外键）
+	Productid int primary key identity(1,1),--供应商商品id
+	Productname varchar(50)not null,        --供应商商品名称
+	Productcontent varchar(100)not null,    --供应商商品简介
+	Productprice varchar(100)not null,      --供应商商品价格
+	Productimg varchar(50) not null,        --供应商商品图片
+	Providerid int references Provider(Providerid)--对应供应商表的供应商id（外键）
 )
-if exists(select * from sys.tables where name='Seller')--卖家表
-drop table Seller
+go
+
+--卖家表	
+if exists(select * from sys.tables where name='Seller')
+	drop table Seller
 go
 create table Seller 
 (
-Sellerid int primary key identity(1,1),--卖家id
-Sellername varchar(50) not null,       --卖家名字
-Sellerstate int default(0) check(Sellerstate=0 or Sellerstate=1)not null,    --(卖家状态默认是0，0是正常，1是冻结)
+	Sellerid int primary key identity(1,1),--卖家id
+	Sellername varchar(50) not null,       --卖家名字
+	Sellerstate int default(0) check(Sellerstate=0 or Sellerstate=1)not null,    --(卖家状态默认是0，0是正常，1是冻结)
 )
-if exists(select * from sys.tables where name='SellerProduct')--卖家商品表
-drop table SellerProduct
+go
+
+
+--卖家商品表
+if exists(select * from sys.tables where name='SellerProduct')
+	drop table SellerProduct
 go
 create table SellerProduct
 (
-Sproductid int primary key identity(1,1),--卖家商品id
-Sproductname varchar(50)not null,        --卖家商品名称
-Sproductcontent varchar(100) not null,   --卖家商品简介
-Sproductprice varchar(100) not null,     --卖家商品价格
-Sproductimg varchar(100) not null,       --卖家商品图片
-Sellerid int references Seller(Sellerid)--对应卖家表的卖家id（外键）
+	Sproductid int primary key identity(1,1),--卖家商品id
+	Sproductname varchar(50)not null,        --卖家商品名称
+	Sproductcontent varchar(100) not null,   --卖家商品简介
+	Sproductprice varchar(100) not null,     --卖家商品价格
+	Sproductimg varchar(100) not null,       --卖家商品图片
+	Sellerid int references Seller(Sellerid)--对应卖家表的卖家id（外键）
 )
-if exists(select * from sys.tables where name='List')--订单表
-drop table List
+go
+
+--订单表
+if exists(select * from sys.tables where name='List')
+	drop table List
 go
 create table List
 (
-Listid int primary key identity(1,1),--订单id
-Sproductid int references SellerProduct(Sproductid),--对应卖家商品表的商品id（外键）
-Liststate int default(0) check(Liststate=0 or Liststate=1 or Liststate=2 or Liststate=3)not null,--(默认值为0，0是待发货，1是已发货，2是已完成，3是退换)订单状态
-Userid int references Users (Userid)--对应用户表的用户id（外键）
+	Listid int primary key identity(1,1),--订单id
+	Sproductid int references SellerProduct(Sproductid),--对应卖家商品表的商品id（外键）
+	Liststate int default(0) check(Liststate=0 or Liststate=1 or Liststate=2 or Liststate=3)not null,--(默认值为0，0是待发货，1是已发货，2是已完成，3是退换)订单状态
+	Userid int references Users (Userid)--对应用户表的用户id（外键）
 )
+go
 
-if exists(select * from sys.tables where name='Buyer')-- 买家个人信息
-drop table Buyer
+
+-- 买家个人信息
+if exists(select * from sys.tables where name='Buyer')
+	drop table Buyer
 go
 create table Buyer
 (
-Buyerid int primary key identity(1,1), --买家id
-Buyername varchar(50) not null,        --买家用户名
-Buyerphone varchar(50) not null,       --买家电话
-Buyeremail varchar(50) not null,       --买家邮箱
-Shname varchar(50) not null,           --收货人姓名
-Shaddress varchar(50) not null,        --收货人地址
-Shphone varchar(50) not null,          --收货人电话
+	Buyerid int primary key identity(1,1), --买家id
+	Buyername varchar(50) not null,        --买家用户名
+	Buyerphone varchar(50) not null,       --买家电话
+	Buyeremail varchar(50) not null,       --买家邮箱
+	Shname varchar(50) not null,           --收货人姓名
+	Shaddress varchar(50) not null,        --收货人地址
+	Shphone varchar(50) not null,          --收货人电话
 )
-if exists(select * from sys.tables where name='Shopping')--买家（购物车表）
-drop table Shopping
+go
+
+--买家（购物车表）
+if exists(select * from sys.tables where name='Shopping')
+	drop table Shopping
 go
 create table Shopping
 (
-Shoppingid int primary key identity(1,1),   --购物车id
-Sproductname varchar(50)not null,        --购物车商品名（对应）（卖家商品名称）
-Sproductcontent varchar(100) not null,   --购物车商品简介（对应）（卖家商品简介）
-Sproductprice varchar(100) not null,     --购物车商品价格（对应）（卖家商品价格）
-Sproductimg varchar(100) not null,       --购物车商品图片（对应）（卖家商品图片）
-Buyerid int references Buyer(Buyerid)    --买家购物车对应买家个人信息（买家id）（外键）
+	Shoppingid int primary key identity(1,1),   --购物车id
+	Sproductname varchar(50)not null,        --购物车商品名（对应）（卖家商品名称）
+	Sproductcontent varchar(100) not null,   --购物车商品简介（对应）（卖家商品简介）
+	Sproductprice varchar(100) not null,     --购物车商品价格（对应）（卖家商品价格）
+	Sproductimg varchar(100) not null,       --购物车商品图片（对应）（卖家商品图片）
+	Buyerid int references Buyer(Buyerid)    --买家购物车对应买家个人信息（买家id）（外键）
 )
-if exists(select * from sys.tables where name='Send')--派送表
-drop table Send
+go
+
+--派送表
+if exists(select * from sys.tables where name='Send')
+	drop table Send
 go
 create table Send
 (
-Sendid int primary key identity(1,1),    --派送id
-Listname varchar(50) not null,           --订单名称
-Listmoney   money not null,              --订单金额
-Listmessage varchar(50) not null,        --订单信息
-Buyerid int  references Buyer (Buyerid)  --对应买家个人信息（买家id）（外键）
+	Sendid int primary key identity(1,1),    --派送id
+	Listname varchar(50) not null,           --订单名称
+	Listmoney   money not null,              --订单金额
+	Listmessage varchar(50) not null,        --订单信息
+	Buyerid int  references Buyer (Buyerid)  --对应买家个人信息（买家id）（外键）
 )
-if exists(select * from sys.tables where name='Manage')--平台管理（公告表）
-drop table Manage
+go
+
+--平台管理（公告表）
+if exists(select * from sys.tables where name='Manage')
+	drop table Manage
 go
 create table Manage
 (
-Manageid  int primary key identity(1,1), --公告id
-Managetitle varchar(50) not null,        --公告标题
-Managecontent varchar(50) not null,      --公告内容
-Managetime time not null,                --公告内容
-Managegrade int check(
-Managegrade=1 or Managegrade=2 or
- Managegrade=3 or Managegrade=4 or
-  Managegrade=5) not null                --公告对应级别(对哪个级别发布)(买家，卖家，供应商，派送，全部)
+	Manageid  int primary key identity(1,1), --公告id
+	Managetitle varchar(50) not null,        --公告标题
+	Managecontent varchar(50) not null,      --公告内容
+	Managetime time not null,                --公告时间
+	Managegrade int check(
+	Managegrade=1 or Managegrade=2 or
+	 Managegrade=3 or Managegrade=4 or
+	  Managegrade=5) not null                --公告对应级别(对哪个级别发布)(买家，卖家，供应商，派送，全部)
 )
-if exists(select * from sys.tables where name='Approval')--级别审批
-drop table Approval
+go
+
+--级别审批
+if exists(select * from sys.tables where name='Approval')
+	drop table Approval
 go
 create table Approval
 (
-Approvalid int primary key identity(1,1), --审批id
-Approvalgrade int not null,               --审批级别
-Approvalcontent varchar(50) not null,     --审批内容
-Approvaltime time not null,               --审批时间
-Userid int references Users(Userid)       --对应用户表（用户id）（外键）
+	Approvalid int primary key identity(1,1), --审批id
+	Approvalgrade int not null,               --审批级别
+	Approvalcontent varchar(50) not null,     --审批内容
+	Approvaltime time not null,               --审批时间
+	Userid int references Users(Userid)       --对应用户表（用户id）（外键）
 )
-if exists(select * from sys.tables where name='Talk')--卖家与买家的交流
-drop table Talk
+go
+
+--卖家与买家的交流
+if exists(select * from sys.tables where name='Talk')
+	drop table Talk
 go
 create table Talk
 (
-Talkid int primary key identity(1,1),     --交流id
-Sellerid int references Seller(Sellerid), --对应卖家表（卖家id）（外键）
-Buyerid int references  Buyer(Buyerid),   --对应买家表（买家id）（外键）	
-Talkcontent varchar(50) not null,         --交流内容
-Talktime time not null,                   --交流时间
+	Talkid int primary key identity(1,1),     --交流id
+	Sellerid int references Seller(Sellerid), --对应卖家表（卖家id）（外键）
+	Buyerid int references  Buyer(Buyerid),   --对应买家表（买家id）（外键）	
+	Talkcontent varchar(50) not null,         --交流内容
+	Talktime time not null,                   --交流时间
 )
 go
 
--- EveryTimeprop.sql
+++ EveryTimeprop.sql
@@ -1,5 +1,7 @@
 use EveryTime
 go
+
+--用户表
 --查询账号密码  根据账号密码进行登录
 if exists(select * from sys.procedures where name='Userslogin')
 drop proc Userslogin
@@ -19,3 +21,116 @@ as
 	select * from Users where Userid=@Userid
 go
 --	exec UsersloginID 1
+
+
+----供应商表
+--全部查询
+if exists(select * from sys.procedures where name='Providerselect')
+drop proc Providerselect
+go
+create proc Providerselect(@Providername varchar(50)='')
+as
+	select * from Provider where Providername like '%'+@Providername+'%'
+go
+--	exec Providerselect 
+
+
+--供应商商品表
+--全部查询
+if exists(select * from sys.procedures where name='ProviderProductselect')
+drop proc ProviderProductselect
+go
+create proc ProviderProductselect(@Productname varchar(50)='')
+as
+	select * from ProviderProduct where Productname like '%'+@Productname+'%'
+go
+--	exec ProviderProductselect 
+
+
+--卖家表
+--全部查询
+if exists(select * from sys.procedures where name='Sellerselect')
+drop proc Sellerselect
+go
+create proc Sellerselect(@Sellername varchar(50)='')
+as
+	select * from Seller where Sellername like '%'+@Sellername+'%'
+go
+--	exec Sellerselect 
+
+
+--卖家商品表
+--全部查询
+if exists(select * from sys.procedures where name='SellerProductselect')
+drop proc SellerProductselect
+go
+create proc SellerProductselect(@Sproductname varchar(50)='')
+as
+	select * from SellerProduct where Sproductname like '%'+@Sproductname+'%'
+go
+--	exec SellerProductselect 
+
+
+--订单表
+--条件查询 (可选)
+if exists(select * from sys.procedures where name='Listselect')
+drop proc Listselect
+go
+create proc Listselect(@Liststate int=-1)
+as
+	if @Liststate<>-1
+		select * from List where Liststate=@Liststate
+	else
+		select * from List
+go
+--	exec SellerProductselect
+
+
+--买家个人信息
+--全部查询
+if exists(select * from sys.procedures where name='Buyerselect')
+drop proc Buyerselect
+go
+create proc Buyerselect
+as
+	select * from Buyer
+go
+--	exec Buyerselect 
+
+
+--买家（购物车表）
+--全部查询
+if exists(select * from sys.procedures where name='Shoppingselect')
+drop proc Shoppingselect
+go
+create proc Shoppingselect(@Sproductname varchar(50)='')
+as
+	select * from Shopping where Sproductname like '%'+@Sproductname+'%'
+go
+--	exec Shoppingselect 
+
+
+--平台管理（公告表）
+--全部查询
+if exists(select * from sys.procedures where name='Manageselect')
+drop proc Manageselect
+go
+create proc Manageselect
+as
+	select * from Manage
+go
+--	exec Shoppingselect 
+
+--级别审批
+--全部查询(可根据级别)
+if exists(select * from sys.procedures where name='Approvalselect')
+drop proc Approvalselect
+go
+create proc Approvalselect(@Approvalgrade int=-1)
+as
+	if @Approvalgrade<>-1
+		select * from Approval
+	else
+		select * from Approval where Approvalgrade =@Approvalgrade
+go
+--	exec Shoppingselect 
